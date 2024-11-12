127f01b0741da839f31d826791aa42d01b0f58e9
mohammed-eladawi <mohammedeladwe2@gmail.com>
2024-11-12 12:24:39 +0400

    added html structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./layouts/Header";
+import Nav from "./layouts/Nav";
+import Main from "./layouts/Main";
+import Footer from "./layouts/Footer";
+import "./App.css";
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <Fragment>
+      <Header>
+        <Nav />
+      </Header>
+      <Main />
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
