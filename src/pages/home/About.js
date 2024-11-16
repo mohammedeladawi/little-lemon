0d51a35a699f3ddebb41e510dbb7614488632d00
shereen mustafa <shereen@gmail.com>
2024-11-16 21:36:05 +0400

    added about styles

--- src/pages/home/About.js
+++ src/pages/home/About.js
@@ -2,17 +2,24 @@ import React from "react";
 import ImgA from "../../assets/images/Mario and Adrian A.jpg";
 import ImgB from "../../assets/images/Mario and Adrian b.jpg";
 import Container from "../../components/grid-system/Container";
+import styles from "./About.module.css";
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "20px",
+};
 
 const About = () => {
   return (
-    <section className="about-section">
-      <Container>
-        <div className="about-info">
-          <div className="titles">
+    <section className={styles["about-section"]}>
+      <Container addStyle={containerStyle}>
+        <div className={styles["about-info"]}>
+          <div className={styles["titles"]}>
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
           </div>
-          <p className="details">
+          <p className={styles["details"]}>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit. Exercitation
             veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
@@ -20,7 +27,7 @@ const About = () => {
             duis enim velit mollit.
           </p>
         </div>
-        <div className="about-imgs">
+        <div className={styles["about-imgs"]}>
           <img src={ImgA} alt="Mario and Adrian" />
           <img src={ImgB} alt="Mario and Adrian" />
         </div>
