f3de76914f22cce01a5a9186fa07a0874529fcba
shereen mustafa <shereen@gmail.com>
2024-11-16 18:53:23 +0400

    added testimonials styles

--- src/pages/home/Testimonials.js
+++ src/pages/home/Testimonials.js
@@ -1,40 +1,45 @@
 import React from "react";
 import TestimonialsCard from "./cards/TestimonialsCard";
 import Container from "../../components/grid-system/Container";
+import styles from "./Testimonials.module.css";
+
+const reviews = [
+  {
+    name: "Mark",
+    avatarSrc: require("../../assets/images/reviewer_1.jpg"),
+    rating: 5,
+    reviewText: "The best!",
+  },
+  {
+    name: "Matilda",
+    avatarSrc: require("../../assets/images/reviewer_2.jpg"),
+    rating: 3,
+    reviewText: "Liked it",
+  },
+  {
+    name: "Nahla",
+    avatarSrc: require("../../assets/images/reviewer_3.jpg"),
+    rating: 4,
+    reviewText: "Delicious!",
+  },
+  {
+    name: "Noah",
+    avatarSrc: require("../../assets/images/reviewer_4.jpg"),
+    rating: 5,
+    reviewText: "Amazing!",
+  },
+];
+
+const containerStyle = { paddingTop: "50px", paddingBottom: "50px" };
 
 const Testimonials = () => {
-  const reviews = [
-    {
-      name: "Mark",
-      avatarSrc: require("../../assets/images/reviewer_1.jpg"),
-      rating: 5,
-      reviewText: "The best!",
-    },
-    {
-      name: "Matilda",
-      avatarSrc: require("../../assets/images/reviewer_2.jpg"),
-      rating: 3,
-      reviewText: "Liked it",
-    },
-    {
-      name: "Nahla",
-      avatarSrc: require("../../assets/images/reviewer_3.jpg"),
-      rating: 4,
-      reviewText: "Delicious!",
-    },
-    {
-      name: "Noah",
-      avatarSrc: require("../../assets/images/reviewer_4.jpg"),
-      rating: 5,
-      reviewText: "Amazing!",
-    },
-  ];
-
   return (
-    <section className="testimonials-section">
-      <Container>
-        <h2>Testimonials</h2>
-        <div className="testimonials">
+    <section className={styles["testimonials-section"]}>
+      <Container addStyle={containerStyle}>
+        <div className={styles["testimonials-header"]}>
+          <h2>Testimonials</h2>
+        </div>
+        <div className={styles["testimonials-body"]}>
           {reviews.map((review, index) => (
             <TestimonialsCard
               key={index}
--- src/pages/home/cards/TestimonialsCard.js
+++ src/pages/home/cards/TestimonialsCard.js
@@ -1,26 +1,26 @@
 import { Star } from "@phosphor-icons/react";
 import React from "react";
+import styles from "./TestimonialsCard.module.css";
 
 const TestimonialsCard = ({ rating, avatarSrc, reviewerName, reviewText }) => {
   let stars = [];
-  console.log(Star);
-  // for (let i = 0; i < rating; i++) {
-  //   stars.push(<Star size={32} />);
-  // }
+  for (let i = 0; i < rating; i++) {
+    stars.push(<Star size={22} weight="fill" />);
+  }
 
   return (
-    <div className="card-testimomials">
-      <div className="card-header">
-        <span className="rating">{stars}</span>
+    <div className={styles["card-testimomials"]}>
+      <div className={styles["card-header"]}>
+        <span className={styles["rating"]}>{stars}</span>
       </div>
 
-      <div className="card-body">
-        <img src={avatarSrc} className="card-avatar" alt="Reviewer Image" />
-        <h5 className="reviewer-name">{reviewerName}</h5>
+      <div className={styles["card-body"]}>
+        <img src={avatarSrc} className={styles["card-avatar"]} alt="Reviewer" />
+        <h5 className={styles["reviewer-name"]}>{reviewerName}</h5>
       </div>
 
-      <div className="card-footer">
-        <p className="card-review">{reviewText}</p>
+      <div className={styles["card-footer"]}>
+        <p className={styles["card-review"]}>{reviewText}</p>
       </div>
     </div>
   );
